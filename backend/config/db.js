fdb55e43e108065c047da8b8203c1cd559077c40
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 05:58:57 +0100

    server setup done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import chalk from 'chalk';
 
 export const connectDB = async(req,res,next) => {
     const url = process.env.DATABASE_URL;
@@ -10,7 +10,7 @@ export const connectDB = async(req,res,next) => {
         useCreateIndex: true,
         useFindAndModify: false
     }).then(() => {
-        console.log("info,"`Succesfully connected to mongoDB`);
+        console.log(chalk.blue("Info : "),chalk.yellow(`Succesfully connected to mongoDB`));
       })
       .catch((error) => {
         console.log("error", error.message);
