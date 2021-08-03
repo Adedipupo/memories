31f8d8a33f1e27f2aa43b31ca64522c9e878f832
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 09:00:39 +0100

    redux

---
+++ frontend/src/reducers/posts.js
@@ -0,0 +1,10 @@
+const reducer = (posts = [],action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+    }
+}
