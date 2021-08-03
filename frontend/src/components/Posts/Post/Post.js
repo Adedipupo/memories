4720dcf81b83cca4397f12d74751906bb282bb79
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 11:10:02 +0100

    show post

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -4,6 +4,7 @@ import { Card,CardMedia, CardActions,CardContent,Button,Typography} from '@mater
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
 
 const Post = ({post}) => {
@@ -11,7 +12,19 @@ const Post = ({post}) => {
     const classes = useStyles();
     return (
         <Card>
-          <CardMedia className={classes.media} image={Post.selectedFile} title={post.title}/>        
+          <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>   
+          <div className={classes.overlay}>
+             <Typography variant="h6">{post.author}</Typography>
+             <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+          </div>     
+          <div className={classes.overlay2}>
+             <Button style={{color:'white'}} size="small" onClick={()=>{}}>
+                <MoreHorizIcon fontSize="default"/>
+             </Button>
+          </div>     
+          <div className={classes.details}>
+            <Typography variant="body2" color="textSecondary">{post.author}</Typography>
+          </div>     
         </Card>
     )
 }
