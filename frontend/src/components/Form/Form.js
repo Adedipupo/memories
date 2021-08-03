81cb07b79bfe9d068130785bcc5da6b39a5824be
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 09:48:27 +0100

    implenting the form to create post

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {TextField,Button,Typography,Paper} from '@material-ui/core'; 
+import useStyles from './styles';
 
 const Form = () => {
+    const classes = useStyles();
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
     return (
         <div>
-            <h2>Form</h2>
+            <Paper className={classes.paper}>
+                <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+
+                </form>
+            </Paper>
         </div>
     )
 }
