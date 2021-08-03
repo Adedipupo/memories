48380f62c68e4ccd23947d01289047a6b5f368bf
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 10:27:17 +0100

    implenting create create post

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios';
 const url = 'http://localhost:5000/api/v1/memories/posts';
 
 export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post('url/create', newPost);
--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './styles';
 import FileBase from 'react-file-base64';
+import {useDispatch} from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({ author: '', title: '', message: '', tags: '', selectedFile: '' })
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -14,6 +17,11 @@ const Form = () => {
     const clear = (e) => {
         e.preventDefault()
     }
+
+    useEffect(() => {
+        dispatch(createPost())
+    }, [dispatch])
+
     return (
         <div>
             <Paper className={classes.paper}>
