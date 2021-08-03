1590a463bd522025b228c729f7816cc6154f7e57
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 05:35:32 +0100

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import logger from "morgan";
+
 
 const app = express();
 
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3030;
 
