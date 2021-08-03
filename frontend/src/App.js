7031dd0fe6ba04b45ca3524ba5455f35f7f389fc
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 08:13:20 +0100

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
-
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/memo.png'
 
 function App() {
   return (
     <Container maxWidth='lg'>
-        <AppBar position="static" color="inherit">
+      <AppBar position="static" color="inherit">
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+         <Container>
+            <Grid container justify="space-between">
 
-        </AppBar>
+            </Grid>
+         </Container>
+      </Grow>
     </Container>
   );
 }
