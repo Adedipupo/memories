6fa464ed9a95dc80e22f9d43f821cd5de2da53dd
Benedict <jokanolaadedipupo@gmail.com>
2021-08-03 09:56:03 +0100

    implenting the form to create post

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {TextField,Button,Typography,Paper} from '@material-ui/core'; 
 import useStyles from './styles';
 
 const Form = () => {
+    cosnt [postData,setPostData] = useState({author: '', title: '', message: '', tags: '',selectedFile: ''})
     const classes = useStyles();
 
     const handleSubmit = (e)=>{
@@ -12,7 +13,15 @@ const Form = () => {
         <div>
             <Paper className={classes.paper}>
                 <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
-
+                 <Typography variant="h6">Creating a Memory</Typography>
+                 <TextField 
+                   name="author"
+                   variant="outlined" 
+                   label="Author" 
+                   fullWidth 
+                   value={postData.author}
+                   onChange={}    
+                   />
                 </form>
             </Paper>
         </div>
