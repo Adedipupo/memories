128c89c2b7d7452caa019dd36cb544c2e1067124
Benedict <jokanolaadedipupo@gmail.com>
2021-08-04 23:38:52 +0100

    update post client

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -9,7 +9,7 @@ import {useSelector} from 'react-redux';
 
 const Form = ({currentId,setCurrentId}) => {
     const [postData, setPostData] = useState({ author: '', title: '', message: '', tags: '', selectedFile: '' })
-    const posts = useSelector(state => state.posts.find((p)=> p._id === currentId))
+    const posts = useSelector(state => currentId ? state.posts.find((p)=> p._id === currentId) : null)
 
     const classes = useStyles();
     const dispatch = useDispatch();
