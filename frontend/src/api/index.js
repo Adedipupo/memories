fa6406eb7ee54c670411b628b8050c49d15bbcae
Benedict <jokanolaadedipupo@gmail.com>
2021-08-04 23:42:45 +0100

    update post client

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const url = 'http://localhost:5000/api/v1/memories/posts';
-const createUrl = 'http://localhost:5000/api/v1/memories/posts/create';
-const updateUrl = 'http://localhost:5000/api/v1/memories/posts/edit/:id';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(`${url}/create`, newPost);
