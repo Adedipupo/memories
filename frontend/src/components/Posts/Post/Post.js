dfc3784a7386e759681447592478a1ed1bc5419c
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 08:15:48 +0100

    client  update done

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -25,8 +25,9 @@ const Post = ({post,setCurrentId}) => {
           <div className={classes.details}>
             <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>`#${tag} `)}</Typography>
           </div>     
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title }</Typography>
           <CardContent>
-            <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+            <Typography variant="body2" gutterBottom>{post.message}</Typography>
           </CardContent>
           <CardActions className={classes.cardActions}>
              <Button size="small" color="primary" onClick={()=>{}}>
