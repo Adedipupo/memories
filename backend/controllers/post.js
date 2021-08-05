622bc76c47ba4aec37380e2f3606c5c7ced3b5d1
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 08:37:27 +0100

    debugging

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -34,13 +34,10 @@ export const getPosts = async(req,res)=>{
 
 export const updatePost = async (req, res) => {
     const { id } = req.params;
-    const { title, message, creator, selectedFile, tags } = req.body;
     
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
-
-    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, req.body, { new: true });
 
     res.json(updatedPost);
 }
