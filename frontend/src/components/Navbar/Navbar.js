d87bc39c1e154ed37ae9b2df9ad438d65ae1effb
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 17:36:02 +0100

    refactoring navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -8,6 +8,7 @@ import memories from '../../images/memo.png'
 const Navbar = () => {
     const classes  = useStyles( );
 
+    const user = null;
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
@@ -15,11 +16,11 @@ const Navbar = () => {
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user?.result ? (
+        {user.result ? (
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
           </div>
         ) : (
           <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
