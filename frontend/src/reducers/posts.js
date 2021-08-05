2692c49817926ee8651faa8cd62df7baeafe1dff
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 09:03:39 +0100

    delete functionality

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default (posts = [],action) => {
     switch (action.type) {
-        case 'UPDATE':
-            return posts.map((post)=>post._id === action.payload._id ? action.payload :post)    
         case 'FETCH_ALL':
             return action.payload;
-        case 'CREATE':
-            return [...posts,action.payload];
+            case 'CREATE':
+                return [...posts,action.payload];
+                case 'UPDATE':
+                    return posts.map((post)=>post._id === action.payload._id ? action.payload :post)    
+        case 'DELETE':
+            return posts.filter((post)=> post._id !== action.payload)   
         default:
             return posts;
     }
