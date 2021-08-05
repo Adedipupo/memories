b96ce292ac466e171647099bf1c43f4531fdf505
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 08:58:22 +0100

    clientside delete

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,3 +5,4 @@ const url = 'http://localhost:5000/api/v1/memories/posts';
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(`${url}/create`, newPost);
 export const updatePost = (id,updatedPost) => axios.patch(`${url}/edit/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/delete/${id}`);
