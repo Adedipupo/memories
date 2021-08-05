2e3769c8378cdad2f399b53c385bcae0fa328aa7
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 11:57:29 +0100

    bug fix

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -36,7 +36,7 @@ const Post = ({post,setCurrentId}) => {
           <CardActions className={classes.cardActions}>
              <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}>
                 <ThumbUpAltIcon fontSize="small" />
-                Like
+                &nbsp; Like &nbsp;
                 {post.likeCount}
              </Button>
 
