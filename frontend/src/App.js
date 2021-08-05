6f1c31018b38c78376762553ac1e4f349cf97ae1
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 17:45:18 +0100

    react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,36 +4,17 @@ import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import {getPosts} from './actions/posts';
 import useStyles from './styles';
-import {useDispatch} from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import Home from './components/Home/Home';
 
 
 function App() {
-  const [currentId,setCurrentId] = useState(null);
-  const classes  = useStyles( );
- const dispatch = useDispatch(); 
 
-
- useEffect(() => {
-  dispatch(getPosts())
- }, [currentId ,dispatch]);
   return (
     <Container maxWidth='lg'>
       <Navbar />
-      <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId}/>
-            </Grid>
-
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Home />
     </Container>
   );
 }
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,14 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { Container, Grow, Grid } from '@material-ui/core';
 import useStyles from './styles';
+import {useDispatch} from 'react-redux';
+
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 
 
 const Home = () => {
-    const [currentId, setCurrentId] = useState(0);
-    const classes  = useStyles( );
+    const [currentId,setCurrentId] = useState(null);
+  const classes  = useStyles( );
+ const dispatch = useDispatch(); 
 
+
+ useEffect(() => {
+  dispatch(getPosts())
+ }, [currentId ,dispatch]);
     return (
         <Grow in>
         <Container>
