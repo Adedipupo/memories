510812cae558fee6dba3f60f7dd5a8d459a4f812
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 09:50:49 +0100

    like a post client

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -6,3 +6,4 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(`${url}/create`, newPost);
 export const updatePost = (id,updatedPost) => axios.patch(`${url}/edit/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/delete/${id}`);
+export const likePost = (id,updatedPost) => axios.patch(`${url}/like/${id}`, updatedPost);
