173d08a7f9d44c929bb053b9a89e49f3f7194b3e
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 17:51:21 +0100

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 
 function App() {
