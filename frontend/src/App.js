561096ff89532d469c9df4a8cce61212c85fdd48
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 12:02:00 +0100

    responsiveness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <Typography className={classes.heading} variant='h4' align='center'>Memories</Typography>
         <img className={classes.image} src={memories} alt="memories" height="80" />
       </AppBar>
       <Grow in>
