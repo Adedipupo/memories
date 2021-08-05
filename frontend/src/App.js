9650b7516e49c91e63dc92bc8fe4af0be9822031
Benedict <jokanolaadedipupo@gmail.com>
2021-08-05 17:52:25 +0100

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/auth" exact component={Auth}/>
         </Switch>
-        <Home />
       </Container>
     </BrowserRouter>
   );
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user.result ? (
+        {user ? (
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
