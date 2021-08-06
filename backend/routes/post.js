e25ab41363f67ccc866e7902c5deee37a85edab6
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 17:34:38 +0100

    auth middleware

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { createPost, deletePost, getPosts, likeAPost, updatePost } from '../controllers/post.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router()
 
 
-router.post('/create', createPost);
+router.post('/create',auth, createPost);
 router.get('/', getPosts);
-router.patch('/edit/:id', updatePost);
-router.delete('/delete/:id', deletePost);
-router.patch('/like/:id', likeAPost);
+router.patch('/edit/:id',auth, updatePost);
+router.delete('/delete/:id',auth, deletePost);
+router.patch('/like/:id',auth, likeAPost);
 
 
 export default router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,13 @@
 
 import express from 'express';
 import { signIn, signUp } from '../controllers/user.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router()
 
 
 router.post('/register', signUp);
-router.post('/login', signIn);
+router.post('/login',auth, signIn);
 
 
 
