d012ca77583028dc838b86ccabfaff917708253c
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 16:52:20 +0100

    user routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import testRoutes from './post.js';
+import postRoutes from './post.js';
+import userRoutes from './user.js';
 
 const router = express.Router();
 
-router.use('/posts',testRoutes)
+router.use('/posts',postRoutes)
+router.use('/users',userRoutes)
 
 
 export default router;
