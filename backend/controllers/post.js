37c18da5bc14a45726dacda8c593ed6063a901db
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 23:13:37 +0100

    user action

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -5,9 +5,9 @@ import { PostMessage } from '../models/postMessage.js';
 
 
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+    const post = req.body;
 
-    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+    const newPostMessage = new PostMessage({ ...post, author: req.userId, createdAt: new Date().toISOString() })
 
     try {
         await newPostMessage.save();
