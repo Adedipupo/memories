2856e59e17db90f52776f8d650cb42b9935c85c5
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 22:44:11 +0100

    login frontend

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,8 +1,11 @@
+import * as api from '../api';
+import { AUTH } from '../constants/actionTypes';
 
 
 export const signup = (formData,history) => async(dispatch) =>{
     try {
-        
+        const {data} = await api.signUp(formData);
+        dispatch({type: AUTH, data})
         history.push('/')
     } catch (error) {
         console.log(error)
@@ -13,7 +16,8 @@ export const signup = (formData,history) => async(dispatch) =>{
 
 export const signin = (formData,history) => async(dispatch) =>{
     try {
-        
+        const {data} = await api.signIn(formData);
+        dispatch({type: AUTH, data})
         history.push('/')
     } catch (error) {
         console.log(error)
