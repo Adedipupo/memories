0dd649abcd1fd58abb5369bc13e529b7e258e1e9
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 07:52:18 +0100

    auth client

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import useStyles from './styles.js';
+
 
 const Auth = () => {
+    const classes = useStyles();
     return (
         <div>
           <h1>Auth</h1>  
