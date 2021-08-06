51bebcf6ccb25ec20ed1977df73007056492b5b0
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 22:37:54 +0100

    login frontend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,3 +7,6 @@ export const createPost = (newPost) => API.post('/posts/create', newPost);
 export const updatePost = (id,updatedPost) => API.patch(`/posts/edit/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/delete/${id}`);
 export const likePost = (id) => API.patch(`/posts/like/${id}`);
+
+export const signUp = (formData) => API.post('/users/register',formData)
+export const signIn = (formData) => API.post('/users/login',formData)
