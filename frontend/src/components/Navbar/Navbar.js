95bb9db7818c63e411d9b1839663a4ef03e86795
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 10:34:31 +0100

    google oauth

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,20 +1,31 @@
 import React,{useState,useEffect} from 'react'
 import { AppBar, Typography, Toolbar, Avatar, Button  } from '@material-ui/core'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 import memories from '../../images/memo.png'
 
 
 const Navbar = () => {
-    const classes  = useStyles( );
+    const classes  = useStyles();
+
+   const history = useHistory(); 
+   const dispatch = useDispatch()
 
     const [user,setUser] = useState(JSON.parse(localStorage.getItem('profile')));
     console.log(user)
 
-    useEffect(() => {
-      const token = user?.token;
-      setUser(JSON.parse(localStorage.getItem('profile')))
-    }, [])
+    const logout = () =>{
+      dispatch({type: 'LOGOUT'})
+      history.push('/')
+    }
+
+    //   useEffect(() => {
+    //   const token = user?.token;
+    //   setUser(JSON.parse(localStorage.getItem('profile')))
+    // }, [])
+
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
@@ -26,7 +37,7 @@ const Navbar = () => {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
           <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,9 +1,12 @@
-import { AUTH } from "../constants/actionTypes";
+import { AUTH, LOGOUT } from "../constants/actionTypes";
 
  const authReducer =  (state={authData: null},action) => {
     switch (action.type) {
         case AUTH:
            localStorage.setItem('profile', JSON.stringify({...action?.data}));
+            return {...state, authData: action?.data};
+        case LOGOUT:
+           localStorage.clear();
             return {...state, authData: action?.data};
         default:
             return state;
