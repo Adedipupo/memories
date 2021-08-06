8e4acc871e7b685282970ad5bbe65cb0c1e3b369
Benedict <jokanolaadedipupo@gmail.com>
2021-08-06 17:36:29 +0100

    user can like a post once

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -52,15 +52,24 @@ export const deletePost = async (req, res) => {
     res.json({ message: "Post deleted successfully." });
 }
 
-export const likeAPost = async(req,res) =>{
-    const {id} = req.params;
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!req.userId) {
+        return res.json({ message: "Unauthenticated" });
+      }
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-
+    
     const post = await PostMessage.findById(id);
 
-    if(post){
-        const updatedPost = await PostMessage.findByIdAndUpdate(id,{likeCount:post.likeCount + 1},{new:true})
-        res.json({data: updatedPost});
+    const index = post.likes.findIndex((id) => id ===String(req.userId));
+
+    if (index === -1) {
+      post.likes.push(req.userId);
+    } else {
+      post.likes = post.likes.filter((id) => id !== String(req.userId));
     }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    res.status(200).json(updatedPost);
 }
