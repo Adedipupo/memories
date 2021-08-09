6583d562f19b1d64ca782641d9f0fee530fb8d0d
Benedict <jokanolaadedipupo@gmail.com>
2021-08-09 08:13:34 +0100

    bug fixes

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoose  from 'mongoose';
+import mongoose from 'mongoose';
 import { PostMessage } from '../models/postMessage.js';
 
 
@@ -12,29 +12,29 @@ export const createPost = async (req, res) => {
     try {
         await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
 }
 
-export const getPosts = async(req,res)=>{
+export const getPosts = async (req, res) => {
     try {
         const posts = await PostMessage.find();
 
-        if(posts.length > 0){
-            return res.status(200).json({message:'Success',posts: posts})
-        }else{
-            return res.status(404).json({message: 'No Post Found'})
+        if (posts.length > 0) {
+            return res.status(200).json({ message: 'Success', posts: posts })
+        } else {
+            return res.status(404).json({ message: 'No Post Found' })
         }
     } catch (error) {
-        res.status(409).json({message: error.message});
+        res.status(409).json({ message: error.message });
     }
 }
 
 export const updatePost = async (req, res) => {
     const { id } = req.params;
-    
+
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
     const updatedPost = await PostMessage.findByIdAndUpdate(id, req.body, { new: true });
@@ -57,18 +57,18 @@ export const likeAPost = async (req, res) => {
 
     if (!req.userId) {
         return res.json({ message: "Unauthenticated" });
-      }
+    }
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-    
+
     const post = await PostMessage.findById(id);
 
-    const index = post.likes.findIndex((id) => id ===String(req.userId));
+    const index = post.likes.findIndex((id) => id === String(req.userId));
 
     if (index === -1) {
-      post.likes.push(req.userId);
+        post.likes.push(req.userId);
     } else {
-      post.likes = post.likes.filter((id) => id !== String(req.userId));
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
     }
     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
     res.status(200).json(updatedPost);
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,12 +4,12 @@ const secret = 'test';
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    let token = req.headers.authorization.split(' ')[1];
     const isCustomAuth = token.length < 500;
 
     let decodedData;
 
-    if (token && isCustomAuth) {      
+    if (token && isCustomAuth) {
       decodedData = jwt.verify(token, secret);
 
       req.userId = decodedData?.id;
@@ -17,7 +17,7 @@ const auth = async (req, res, next) => {
       decodedData = jwt.decode(token);
 
       req.userId = decodedData?.sub;
-    }    
+    }
 
     next();
   } catch (error) {
